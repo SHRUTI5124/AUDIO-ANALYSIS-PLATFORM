7a495587f576ebec02cdd6315398303949349b08
Shruti Mishra <shrutibca5124@gmail.com>
2023-05-08 16:08:25 +0530

    sentiment analysis

--- analyse_sentiments.py
+++ analyse_sentiments.py
@@ -1,44 +1,31 @@
-import speech_recognition as sr
-import librosa
-import librosa.display
-import numpy as np
-import pandas as pd
-from tensorflow.keras.models import load_model
-import nltk
-from nltk.sentiment import SentimentIntensityAnalyzer
+import wave
+from textblob import TextBlob
+from file_conversion import convert_to_wav
+from transcript import transcript
+import os
 
-# Load the audio file
-audio_file = r'static\audios\Recording.m4a'
 
-def analyze_sentiment():                  # to analyze the sentiment emotions of audio
-    r = sr.Recognizer()
-    with sr.AudioFile(audio_file) as source:
-        audio = r.record(source)
+def analyse_sentiments(audio_file_path):
 
-    # Convert speech to text
-    text = r.recognize_google(audio)
+    # check file type and if not wav, convert to wav
+    if audio_file_path.endswith('.wav'):
+        wav_path = audio_file_path
+    else:
+        wav_path = audio_file_path[:-4] + '.wav'
+        convert_to_wav(audio_file_path, wav_path)
+    
+    # get transcript
+    transcript_text = transcript(wav_path)
+    blob = TextBlob(transcript_text)
 
-    # Perform sentiment analysis using NLTK's SentimentIntensityAnalyzer
-    sid = SentimentIntensityAnalyzer()
-    sentiment_scores = sid.polarity_scores(text)
+    # get sentiment
+    sentiment = blob.sentiment.polarity
+    subjectivity = blob.sentiment.subjectivity
 
-    # Load the ML model for emotion detection
-    model_file = 'path/to/model.h5'
-    model = load_model(model_file)
+    print(sentiment, subjectivity)
+    return sentiment
 
-    # Extract audio features
-    y, sr = librosa.load(audio_file)
-    mfccs = librosa.feature.mfcc(y, sr=sr, n_mfcc=40)
-    mfccs_scaled = np.mean(mfccs.T,axis=0)
 
-    # Predict the emotion using the ML model
-    emotion_dict = {0: 'neutral', 1: 'calm', 2: 'happy', 3: 'sad', 4: 'angry', 5: 'fearful', 6: 'disgust', 7: 'surprised'}
-    emotion_pred = model.predict_classes(np.array([mfccs_scaled]))
-    emotion = emotion_dict[emotion_pred[0]]
+if __name__ == "__main__":
+    analyse_sentiments(r'C:\Users\hp\OneDrive\Documents\web_app1\recording.wav')
 
-    # Print the sentiment and emotion
-    print('Sentiment:', sentiment_scores)
-    print('Emotion:', emotion)
-
-if __name__=='__main__':
-    analyze_sentiment() 

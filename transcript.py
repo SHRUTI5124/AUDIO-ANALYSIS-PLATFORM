576c3cd39af0a3d3ff62f8ba98be3b2b8d07c027
Shruti Mishra <shrutibca5124@gmail.com>
2023-05-08 15:28:28 +0530

    edit

--- transcript.py
+++ transcript.py
@@ -1,4 +1,17 @@
 from leopard import Leopard 
-from cheetah import Transcriber
+from pvcheetah import Transcriber
 
+audio_file = r"static\audios\sunrise-anna-li-sky-wav-8476.mp3"
 
+#to transcript the audio file
+def transcript(audio_file):
+    #transcript audio file
+    transcript = Transcriber(audio_file)
+    transcript.transcript()
+    #load the transcript file
+    leopard = Leopard('transcript.txt')
+    #return the transcript
+    return leopard.get_transcript()
+
+if __name__ == "__main__":
+    transcript(audio_file)
